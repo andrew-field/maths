7fd2096d09f8da577756dc57819a831d490a3a9d
Andrew Field <27866671+andrew-field@users.noreply.github.com>
2023-02-19 14:05:55 +0200

    Fix spelling

--- prime_numbers.go
+++ prime_numbers.go
@@ -7,70 +7,7 @@ package maths
 
 import "context"
 
-// Send the sequence 2, 3, 4, ... to channel 'ch'.
-func generate(ctx context.Context, ch chan<- int) {
-	for i := 2; ; i++ {
-		select {
-		case ch <- i: // Send 'i' to channel 'ch'.
-		case <-ctx.Done():
-			return
-		}
-	}
-}
-
-// Copy the values from channel 'in' to channel 'out',
-// removing those divisible by 'prime'.
-func filter(ctx context.Context, in <-chan int, out chan<- int, prime int) {
-	for {
-		var i int
-		select {
-		case i = <-in: // Receive value from 'in'.
-		case <-ctx.Done():
-			return
-		}
-
-		if i%prime != 0 {
-			select {
-			case out <- i: // Send 'i' to 'out'.
-			case <-ctx.Done():
-				return
-			}
-		}
-	}
-}
-
-// GetPrimeNumbers returns a channel from which to siphon off the prime numbers in order, as needed.
-// The prime sieve: Daisy-chain Filter processes.
-func GetPrimeNumbers() (<-chan int, chan<- bool) {
-	ch := make(chan int) // Create a new channel.
-	ctx, cancel := context.WithCancel(context.Background())
-	go generate(ctx, ch) // Launch Generate goroutine.
-
-	primeCh := make(chan int) // Create return channel.
-	doneCh := make(chan bool) // Create done channel.
-	go func() {
-		<-doneCh
-		cancel()
-	}()
-
-	go func() {
-		for {
-			prime := <-ch
-			select {
-			case primeCh <- prime:
-			case <-ctx.Done():
-				return
-			}
-			ch1 := make(chan int)
-			go filter(ctx, ch, ch1, prime)
-			ch = ch1
-		}
-	}()
-
-	return primeCh, doneCh
-}
-
-// GetPrimeNumbersBelow fills a channel with the prime numbers below |n|.
+// GetPrimeNumbersBelow fills a channel with the prime numbers below |n|. Uses a euclidean sieve.
 func GetPrimeNumbersBelow(n int) <-chan int {
 	n = Abs(n)
 
@@ -102,4 +39,68 @@ func GetPrimeNumbersBelow(n int) <-chan int {
 	}()
 
 	return primeChannel
+}
+
+// GetPrimeNumbers returns a channel from which to siphon off the prime numbers in order, as needed.
+// Send a boolean to the Done channel when finished.
+// The prime sieve: Daisy-chain Filter processes.
+func GetPrimeNumbers() (<-chan int, chan<- bool) {
+	ch := make(chan int) // Create a new channel.
+	ctx, cancel := context.WithCancel(context.Background())
+	go generate(ctx, ch) // Launch Generate goroutine.
+
+	primeCh := make(chan int) // Create return channel.
+	doneCh := make(chan bool) // Create done channel.
+	go func() {
+		<-doneCh
+		cancel()
+	}()
+
+	go func() {
+		for {
+			prime := <-ch
+			select {
+			case primeCh <- prime:
+			case <-ctx.Done():
+				return
+			}
+			ch1 := make(chan int)
+			go filter(ctx, ch, ch1, prime)
+			ch = ch1
+		}
+	}()
+
+	return primeCh, doneCh
+}
+
+// Send the sequence 2, 3, 4, ... to channel 'ch'.
+func generate(ctx context.Context, ch chan<- int) {
+	for i := 2; ; i++ {
+		select {
+		case ch <- i: // Send 'i' to channel 'ch'.
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+// Copy the values from channel 'in' to channel 'out',
+// removing those divisible by 'prime'.
+func filter(ctx context.Context, in <-chan int, out chan<- int, prime int) {
+	for {
+		var i int
+		select {
+		case i = <-in: // Receive value from 'in'.
+		case <-ctx.Done():
+			return
+		}
+
+		if i%prime != 0 {
+			select {
+			case out <- i: // Send 'i' to 'out'.
+			case <-ctx.Done():
+				return
+			}
+		}
+	}
 }
