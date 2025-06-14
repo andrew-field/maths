1499b78bdcc7c7918fae9f4818f8f5dfcc277980
andrew-field <27866671+andrew-field@users.noreply.github.com>
2025-06-14 21:24:42 +0300

    feat: refactor digit functions to take type parameters (#59)
    * feat: refactor digit function to take type parameters
    
    * fix: clear up some repeated code

--- digits.go
+++ digits.go
@@ -1,72 +1,36 @@
 package maths
 
 import (
+	"fmt"
 	"math/big"
-	"slices"
-	"strconv"
 )
 
-// NumberOfDigits returns the number of digits of an integer. Uses integer-string conversion.
-func NumberOfDigits(x int) int {
-	s := len(strconv.Itoa(x))
+// NumberOfDigits returns the number of digits of an integer or big.Int. Uses integer-string conversion.
+func NumberOfDigits[T int | *big.Int](x T) int {
+	s := fmt.Sprintf("%d", x) // Convert the number to a string.
 
-	if x < 0 { // Can not write x = -x because x is an int and math.MinInt is not representable as a positive int.
-		return s - 1
+	length := len(s)
+
+	// If the number is negative, skip the '-' sign.
+	if s[0] == '-' {
+		return length - 1
 	}
 
-	return s
-}
-
-// NumberOfDigitsBig returns the number of digits of a big.Int. Uses integer-string conversion.
-func NumberOfDigitsBig(x *big.Int) int {
-	s := len(x.String())
-
-	if x.Sign() == -1 {
-		return s - 1
-	}
-	return s
+	return length
 }
 
 // GetDigits returns a slice filled with the digits of x in the same order (starting with the largest magnitude numbers, left to right).
-func GetDigits(x int) []int {
+func GetDigits[T int | *big.Int](x T) []int {
 	digits := make([]int, 0)
-	if x == 0 {
-		digits = append(digits, 0)
+	s := fmt.Sprintf("%d", x) // Convert the number to a string.
+
+	// If the number is negative, skip the '-' sign.
+	if s[0] == '-' {
+		s = s[1:]
 	}
 
-	if x < 0 {
-		digits = slices.Insert(digits, 0, -(x % 10)) // Can successfully handle math.MinInt
-		x /= -10
-	}
-
-	// 456/10 = 45 with int.
-	for x > 0 {
-		digits = slices.Insert(digits, 0, x%10)
-		x /= 10
-	}
-
-	return digits
-}
-
-// GetDigitsBig returns a slice filled with the digits of x in the same order (starting with the largest magnitude numbers, left to right).
-func GetDigitsBig(x *big.Int) []int {
-	digits := make([]int, 0)
-	if x.Sign() == 0 {
-		digits = append(digits, 0)
-	}
-
-	// Uses a new variable, altNumber, so as to not change the original number.
-	altNumber := new(big.Int)
-
-	// Make number positive.
-	altNumber.Abs(x)
-
-	ten := big.NewInt(10)
-	var digit big.Int
-
-	for altNumber.Sign() == 1 { // For altNumber > 0.
-		altNumber.QuoRem(altNumber, ten, &digit) // Go has this handy function. Sets altNumber to altNumber / 10 and sets digit to altNumber mod 10 (the last digit).
-		digits = slices.Insert(digits, 0, int(digit.Int64()))
+	for _, val := range s {
+		digits = append(digits, int(val-'0')) // Convert each character to its integer value by subtracting the ASCII value of '0'.
 	}
 
 	return digits
