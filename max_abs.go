eb7bdbe3e408e4c63f9e39cd772a2c3baa2d7d12
Andrew Field <27866671+andrew-field@users.noreply.github.com>
2025-07-07 23:47:22 +0300

    feat: refactor pow tests

--- max_abs.go
+++ max_abs.go
@@ -22,7 +22,7 @@ var ErrOverflowDetected = errors.New("arithmetic overflow detected")
 var ErrAbsoluteValueOfMinInt = fmt.Errorf("can not calculate the absolute value of math.MinInt and store in an int variable: %w", ErrOverflowDetected)
 
 // Abs returns |x|. Returns an error if calculating the absolute value of math.MinInt.
-// In this case, use Abs() from the math/big package.
+// In this case, consider *big.Int.Abs() from the math/big package.
 func Abs(x int) (int, error) {
 	if x == math.MinInt {
 		return 0, ErrAbsoluteValueOfMinInt
--- pow.go
+++ pow.go
@@ -8,7 +8,7 @@ import (
 // Pow returns the x^|y|.
 // Returns 1, nil for all x and y when y is 0 or x is 1.
 // If an overflow error is detected when the numbers get too large, the function returns 0, ErrOverflowDetected.
-// In this case, use *bigInt.Exp() from the math/big package.
+// In this case, consider *big.Int.Exp() from the math/big package.
 func Pow(x, y int) (int, error) {
 	if y == 0 || x == 1 {
 		return 1, nil
@@ -16,7 +16,7 @@ func Pow(x, y int) (int, error) {
 
 	absY, err := Abs(y)
 	if err != nil {
-		return 0, fmt.Errorf("failed to get Abs(%d): %w", y, err)
+		absY = math.MaxInt // The absolute value of math.MinInt is not important as there will be an overflow error.
 	}
 
 	result := x
